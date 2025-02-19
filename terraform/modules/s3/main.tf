5e10f8ccc5b0aa695d8490a1cc4cf96c168e9754
am2308 <b110020510@gmail.com>
2025-02-19 11:33:21 +0000

    feat:added new code

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_public_access_block" "log_bucket_restrictions" {
   bucket = aws_s3_bucket.log_bucket.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
